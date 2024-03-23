a63e46bf3ce3d111b2bfcc03d8ac682800dfa7aa
xXxinsanexXx <shreyanshsaha19@proton.me>
2024-03-23 17:10:23 +0530

    (#007) : Improved string-mod.c

--- cs50/week2/lecture-code-dumps/string-mod.c
+++ cs50/week2/lecture-code-dumps/string-mod.c
@@ -1,22 +1,58 @@
-#include <stdio.h>
+/*
+ * This program converts all lowercase characters to uppercase in a string literal implemented using character array
+ *
+ *
+ * While loop method:
+ * iterator variable checks for '\0' (NUL) char to determine the end of string. Alternatively size of string could also be used for the same.
+ *
+ *
+ * For loop method:
+ * To calculate size of string, strlen() fn is used alternatively the following could be use:
+ * int length_of_string = (sizeof(array_of_string) / sizeof(array_of_string[0]))
+ *
+ * */
 
+
+#include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
+    
+    // Array of char to store string
     char arr[] = "HelLo";
     int n = 0;
+
+
+// Using while loop
+#if 0 
     
-
-
+    // Loop until end of string
     while (arr[n] != '\0') {
         
+        // Apply condition only is character is lowercase
         if ('a' <= arr[n] && arr[n] <= 'z') {
-            arr[n] = arr[n] - ('a' - 'A'); 
+            arr[n] = arr[n] - ('a' - 'A');
         }
         n++;
     }
-   
-    printf("%s\n", arr);
 
+
+// Using for loop
+#else
+    
+    // Loop until end of string
+    for (int i=0, len=strlen(arr); i<len; i++) {
+
+        // Apply condition only is character is lowercase
+        if ('a' <= arr[i] && arr[i] <= 'z') {
+            arr[i] = arr[i] - ('a' - 'A');
+        }
+    }
+
+#endif   
+
+    printf("%s\n", arr);
+    
     return 0;
 }
